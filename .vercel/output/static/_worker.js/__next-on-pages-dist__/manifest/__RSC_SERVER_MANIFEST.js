bd0b1b66f10ff482e09490a6b55921ec8b57c807
Christian Ndu <christiannduh@gmail.com>
2026-01-02 02:49:40 +0100

    build: generate Vercel deployment artifacts and Next.js environment types.

---
+++ .vercel/output/static/_worker.js/__next-on-pages-dist__/manifest/__RSC_SERVER_MANIFEST.js
@@ -0,0 +1,5 @@
+var _={},t=(e,n,o)=>(_.__RSC_SERVER_MANIFEST=e.__RSC_SERVER_MANIFEST=`{
+  "node": {},
+  "edge": {},
+  "encryptionKey": "HIkFfxuDQ9rfgXGUELv4X49Y5t2fwI9K7IXQ1Y4+o5A="
+}`,_);export{t as __getNamedExports};
